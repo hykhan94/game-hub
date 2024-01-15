8d446cc09113b6da59dd2502ad703019151d5d99
hykhan94 <hykhan94@gmail.com>
2024-01-15 05:15:39 +0500

    initial

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "bbf5a3daadbd43c49234ced1d1456ed2",
   },
 });

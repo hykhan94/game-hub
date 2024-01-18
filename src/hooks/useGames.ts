a28ad9b7c2e7e1bd71a8cbd5e59ccd41fd13a99d
hykhan94 <hykhan94@gmail.com>
2024-01-18 06:30:10 +0500

    Exercise - use time calculations

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,9 +1,8 @@
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import ms from "ms";
 import { GameQuery } from "../App";
-import APIClient from "../services/api-client";
-import { FetchResponse } from "../services/api-client";
+import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
-import { all } from "axios";
 
 const apiClient = new APIClient<Game>("/games");
 
@@ -32,7 +31,7 @@ const useGames = (gameQuery: GameQuery) =>
     getNextPageParam: (lastPage, allPages) => {
       return lastPage ? allPages.length + 1 : undefined;
     },
-    staleTime: 24 * 60 * 60 * 1000, //24h
+    staleTime: ms("24h"), //24h
   });
 
 export default useGames;
